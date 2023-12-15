2a264a8a3a8ea44811fc1529c241076d0d04a2ae
Jeff 'Dukk' Snyder <152240878+Dukkering@users.noreply.github.com>
2023-12-15 01:00:34 -0600

    Update AsteriskLogger.java /w comments

--- src/week05project/AsteriskLogger.java
+++ src/week05project/AsteriskLogger.java
@@ -13,14 +13,15 @@ public class AsteriskLogger implements Logger{
 
 	@Override
 	public void error(String error) {
-		int length = error.length() + 7;
-		String border = getAsteriskBorder(length);
+		int length = error.length() + 7; // Variable to get the length of the message + 7 spaces for 'Error: '
+		String border = getAsteriskBorder(length); // Calling on the method to get the proper top and bottom border length
 		System.out.println(border);
 		System.out.println("***Error: " + error + "***");
 		System.out.println(border);
 		
 	}
-	
+	// Method to make the asterisks top and bottom border to equal the rough length of the error message
+	// Uses the for loop to place one asterisk per character in the length of the error message, plus 6 for some padding.
 	private String getAsteriskBorder(int length) {
 		StringBuilder border = new StringBuilder();
 		for (int i = 0; i < length + 6; i++) {
