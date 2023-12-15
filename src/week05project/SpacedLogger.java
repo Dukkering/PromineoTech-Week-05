feecb998bd9e78d2acba9d15b5ac8aadafe89fad
Jeff 'Dukk' Snyder <152240878+Dukkering@users.noreply.github.com>
2023-12-15 01:02:29 -0600

    Update SpacedLogger.java

--- src/week05project/SpacedLogger.java
+++ src/week05project/SpacedLogger.java
@@ -5,11 +5,13 @@ public class SpacedLogger implements Logger{
 	@Override
 	public void log(String log) {
 		StringBuilder spacedMessage = new StringBuilder();
+		// Uses a for loop to go through the length of the log string and sticks a " " space after every character
 		
 		for (int i = 0; i < log.length(); i++) {
 			spacedMessage.append(log.charAt(i)).append(" ");
 		}
 		System.out.println(spacedMessage.toString().trim());
+		// .trim() at the end to remove the last space for cleanliness sake, suggested by our dear ChatGPT. Thanks chatGPT!
 		
 		
 	}
